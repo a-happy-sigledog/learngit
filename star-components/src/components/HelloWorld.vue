885e3ee27e3c5e40914755f9e7239ab4c473f94f
SAOyeDOG <1041153622@qq.com>
2018-06-24 08:55:38 +0800

    a vue star components

---
+++ star-components/src/components/HelloWorld.vue
@@ -0,0 +1,24 @@
+<template>
+ <div>
+   <star :size='48' :score='4.5'></star>
+ </div>
+</template>
+
+<script>
+import Star from './star'
+ export default {
+   data () {
+     return {
+
+     }
+   },
+   components: {
+    Star,
+   }
+ }
+</script>
+
+<style lang='scss' scoped>
+
+ 
+</style>
